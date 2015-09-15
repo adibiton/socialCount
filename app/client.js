8afcc06604d80f192461238180362faea1235141
adi.biton <adi.biton@sap.com>
2015-09-15 09:20:15 +0300

    remove client.js from index.html

--- app/client.js
+++ app/client.js
@@ -1,24 +1,24 @@
 'use strict';
 
 (function(){
-	//  myApp.api.getSocialCount('facebook','http://walla.co.il')
-	//  								.then(function (result){
-	//  										console.log(result);
-	//  								})
-	//  								.catch(function (result){
-	//  									console.error(result);
-	//  								});
-	// myApp.api.getSocialCount('twitter','http://walla.co.il')
-	//   								.then(function (result){
-	//   										console.log(result);
-	//   								});
+	 myApp.api.getSocialCount('facebook','http://walla.co.il')
+	 								.then(function (result){
+	 										console.log(result);
+	 								})
+	 								.catch(function (result){
+	 									console.error(result);
+	 								});
+	myApp.api.getSocialCount('twitter','http://walla.co.il')
+	  								.then(function (result){
+	  										console.log(result);
+	  								});
 	
-	// myApp.api.getSocialCount('pinterest','http://walla.co.il')
-	//   								.then(function (result){
-	//   										console.log(result);
-	//   								});
+	myApp.api.getSocialCount('pinterest','http://walla.co.il')
+	  								.then(function (result){
+	  										console.log(result);
+	  								});
 
-	myApp.api.getSocialCount('facebook',['ifjdsodo', 'http://ynet.co.il', 'http://yahoo.com'])
+	myApp.api.getSocialCount('facebook',['http://www.google.com', 'http://ynet.co.il', 'http://yahoo.com'])
 	  								.then(function (result){
 	  								 		console.log(result);
 	  								 })
@@ -26,9 +26,9 @@
 	  									console.error(result);
 	  								});
 
-	// myApp.api.getSocialCount(['facebook', 'twitter'],['http://walla.co.il', 'http://ynet.co.il', 'http://yahoo.com'])
-	// 								.then(function (result){
-	// 								 		console.log(result);
-	// 								 });
+	myApp.api.getSocialCount(['facebook', 'twitter'],['http://walla.co.il', 'http://ynet.co.il', 'http://yahoo.com'])
+									.then(function (result){
+									 		console.log(result);
+									 });
 }());
 
